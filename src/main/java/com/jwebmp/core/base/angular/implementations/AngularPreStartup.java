fead89d70c163a8e9d9ef002581ad771c3197142
GedMarc <marc.magon@gmail.com>
2025-03-02 21:17:45 +0200

    Pre Startup items update

--- src/main/java/com/jwebmp/core/base/angular/implementations/AngularPreStartup.java
+++ src/main/java/com/jwebmp/core/base/angular/implementations/AngularPreStartup.java
@@ -1,12 +1,16 @@
 package com.jwebmp.core.base.angular.implementations;
 
 import com.guicedee.guicedinjection.interfaces.IGuicePreStartup;
+import io.vertx.core.Future;
+
+import java.util.List;
 
 public class AngularPreStartup implements IGuicePreStartup<AngularPreStartup>
 {
     @Override
-    public void onStartup()
+    public List<Future<Boolean>> onStartup()
     {
         System.setProperty("BIND_JW_PAGES", "false");
+        return List.of(Future.succeededFuture(true));
     }
 }
