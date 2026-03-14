20a59e1bd65d643c6d7cbde63c0ebba2a5009001
GedMarc <ged_marc@hotmail.com>
2026-03-14 04:24:17 +0200

    Update Prestartups to be typed

--- src/main/java/com/jwebmp/core/base/angular/implementations/SearchPathConfigurator.java
+++ src/main/java/com/jwebmp/core/base/angular/implementations/SearchPathConfigurator.java
@@ -3,7 +3,7 @@ package com.jwebmp.core.base.angular.implementations;
 import com.guicedee.client.services.IGuiceConfig;
 import com.guicedee.client.services.lifecycle.IGuiceConfigurator;
 
-public class SearchPathConfigurator implements IGuiceConfigurator
+public class SearchPathConfigurator implements IGuiceConfigurator<SearchPathConfigurator>
 {
 	@Override
 	public IGuiceConfig<?> configure(IGuiceConfig<?> config)
